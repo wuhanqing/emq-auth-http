ae325918ec461030b92a36ee4ecfaf0887391573
hanqingwu <hanqingwu@hengtiansoft.com>
2017-01-09 11:47:44 +0800

    no message

--- src/emq_auth_http.erl
+++ src/emq_auth_http.erl
@@ -55,7 +55,7 @@ description() -> "Authentication by HTTP API".
 %% Is Superuser?
 %%--------------------------------------------------------------------
 
--spec(is_superuser(undefined | #http_request{}, mqtt_client()) -> boolean()).
+-spec(is_superuser(undefined | #http_request{}, mqtt_client(), string()) -> boolean()).
 is_superuser(undefined, _MqttClient) ->
     false;
 is_superuser(#http_request{method = Method, url = Url, params = Params}, MqttClient, Password) ->
